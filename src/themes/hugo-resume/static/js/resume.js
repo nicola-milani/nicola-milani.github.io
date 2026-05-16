51fc0ae5170c80313da55e92f01c5b49cde50c71
Nicola Milani <nicoweb313@gmail.com>
2026-05-16 17:35:57 +0200

    locale font

--- src/themes/hugo-resume/static/js/resume.js
+++ src/themes/hugo-resume/static/js/resume.js
@@ -1,38 +1,46 @@
-(function ($) {
-  "use strict"; // Start of use strict
+(function () {
+  'use strict';
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+  function init() {
+    // Smooth scroll to in-page anchors
+    document.querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])').forEach((link) => {
+      link.addEventListener('click', function (e) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+            location.hostname === this.hostname) {
+          const target = document.querySelector(this.hash) ||
+                          document.querySelector('[name="' + this.hash.slice(1) + '"]');
+          if (target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      });
+    });
+
+    // Close the responsive navbar after a link is clicked
+    document.querySelectorAll('.js-scroll-trigger').forEach((link) => {
+      link.addEventListener('click', () => {
+        const openNav = document.querySelector('.navbar-collapse.show');
+        if (openNav) {
+          bootstrap.Collapse.getOrCreateInstance(openNav).hide();
+        }
+      });
+    });
+
+    // Highlight nav items while scrolling
+    if (document.querySelector('#sideNav')) {
+      new bootstrap.ScrollSpy(document.body, { target: '#sideNav' });
     }
-  });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  const sideNav = document.body.querySelector('#sideNav');
-  if (sideNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: '#sideNav'
+    // Enable Bootstrap tooltips
+    document.querySelectorAll('[data-toggle="tooltip"]').forEach((el) => {
+      new bootstrap.Tooltip(el);
     });
   }
 
-
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-
-
-})(jQuery); // End of use strict
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
